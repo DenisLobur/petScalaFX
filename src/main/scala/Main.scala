d4b4d49d301cb54f20ce5b2babd0322cc2524c96
Denis <denisdenisdenis@bigmir.net>
2018-01-21 21:29:13 +0200

    add Poll table

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import java.io.File
+
 import scalafx.Includes._
 import scalafx.application
 import scalafx.application.JFXApp
@@ -78,6 +80,27 @@ object Main extends JFXApp {
     vBox.children ++= List(topText, bottomText)
 
     vBox
+  }
+
+  readFile()
+  // To split string by commas not touching commas inside double quotes
+  val outQuotesRegex = ",(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)"
+
+  private def readFile(): Unit = {
+//    val resourcesPath = getClass.getResource("/data")
+    val bufferedSource = io.Source.fromResource("june2017")
+//    val folder = new File(resourcesPath.getPath)
+//    if (folder.exists && folder.isDirectory)
+//      folder.listFiles
+//        .toIterator
+//          .filter(_.isFile)
+//        .foreach(file => println(file.getName))
+    for (line <- bufferedSource.getLines) {
+      val cols = line.split(",(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)")
+      cols.foreach(entry => println(entry))
+      //println(s"${cols(0)}|${cols(1)}|${cols(2)}|${cols(3)}")
+    }
+    bufferedSource.close
   }
 
 }
---
+++ src/main/scala/model/Poll.scala
@@ -0,0 +1,37 @@
+package model
+
+import scala.concurrent.Future
+import slick.jdbc.PostgresProfile.api._
+
+case class Poll(id: Int, city: String, experience: Double, salary: Int, position: String, language: String)
+
+class PollTable(tag: Tag) extends Table[Poll](tag, "poll") {
+  val id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+  val city = column[String]("city")
+  val experience = column[Double]("experience")
+  val salary = column[Int]("salary")
+  val position = column[String]("position")
+  val language = column[String]("language")
+
+  def * = (id, city, experience, salary, position, language) <> (Poll.apply _ tupled, Poll.unapply)
+}
+
+object PollTable {
+  val table = TableQuery[PollTable]
+}
+
+class CompanyRepository(db: Database) {
+  val table = TableQuery[PollTable]
+
+  def create(poll: Poll): Future[Poll] = {
+    db.run(table returning table += poll)
+  }
+
+  def update(poll: Poll): Future[Int] = {
+    db.run(table.filter(_.id === poll.id).update(poll))
+  }
+
+  def delete(poll: Poll): Future[Int] = {
+    db.run(table.filter(_.id === poll.id).delete)
+  }
+}
